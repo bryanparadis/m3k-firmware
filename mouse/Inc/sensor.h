98af62e1c3afb71cde6edb2b2a9848e405e68ec4
Bryan Paradis <bryan.paradis@gmail.com>
2026-04-18 01:51:37 -0700

    cleanup: move last function from SPI1 to SPIx

--- mouse/Inc/sensor.h
+++ mouse/Inc/sensor.h
@@ -97,10 +97,10 @@
 
 	static inline uint8_t spi_sendrecv(uint8_t b)
 	{
-		while (!(SPI1->SR & SPI_SR_TXE));
-		*(__IO uint8_t *)&SPI1->DR = b;
-		while (!(SPI1->SR & SPI_SR_RXNE));
-		return *(__IO uint8_t *)&SPI1->DR;
+		while (!(SPIx->SR & SPI_SR_TXE));
+		*(__IO uint8_t *)&SPIx->DR = b;
+		while (!(SPIx->SR & SPI_SR_RXNE));
+		return *(__IO uint8_t *)&SPIx->DR;
 	}
 
 #define spi_recv(x) spi_sendrecv(0)
